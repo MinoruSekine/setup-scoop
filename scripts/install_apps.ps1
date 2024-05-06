3b058c07a0efe5828764cbf9bf82a59ef3d3ea87
Minoru Sekine <Minoru.Sekine.JP@gmail.com>
2024-05-06 14:08:58 +0900

    feat: Improved some log output. (#51)

--- scripts/install_apps.ps1
+++ scripts/install_apps.ps1
@@ -2,16 +2,19 @@ param(
     [Parameter()]
     [string]$apps_string = $(throw "apps parameter is necessary.")
 )
-Write-Host parameter: $apps_string
+
+Import-Module "$($PSScriptRoot)\LogModule"
+
+WriteSetupScoopLog "parameter: ${apps_string}"
 [string[]] $apps = @()
 if ($apps_string) {
     $apps = $apps_string.Split(" ")
 }
-Write-Host apps: $apps
+WriteSetupScoopLog "apps: ${apps}"
 foreach($app in $apps) {
     if ($app -inotmatch "^\w[\w/.@]+$") {
         Write-Error "Illegal app name `"$app`"." -ErrorAction Stop
     }
-    Write-Host app: $app
+    WriteSetupScoopLog "Installing `"${app}`""
     scoop install $app
 }
