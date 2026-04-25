463ca63f967245dbf6d82d2cc7ccd3fb1d56912d
Minoru Sekine <Minoru.Sekine.JP@gmail.com>
2026-04-25 19:14:14 +0900

    refactoring: More clear and strict params and results types.

--- scripts/AppNameModule/AppNameModule.psm1
+++ scripts/AppNameModule/AppNameModule.psm1
@@ -1,5 +1,8 @@
 function Test-AppName {
+    [CmdletBinding()]
+    [OutputType([bool])]
     param(
+        [Parameter(Mandatory = $true)]
         [string]$AppName
     )
     return $AppName -imatch "\A\w[\w/.@-]*\z"
--- scripts/LogModule/LogModule.psm1
+++ scripts/LogModule/LogModule.psm1
@@ -1,5 +1,7 @@
 function Write-SetupScoopLog {
+    [CmdletBinding()]
     param(
+        [Parameter(Mandatory = $true)]
         [string]$LogMessage
     )
     Write-Information "setup-scoop: $LogMessage" -InformationAction Continue
