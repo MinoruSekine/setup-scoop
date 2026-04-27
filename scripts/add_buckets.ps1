84b5d06ef935b056731b55e12144ad4fdd2261b1
Minoru Sekine <Minoru.Sekine.JP@gmail.com>
2026-04-27 11:32:59 +0900

    chore: Improve log readability.

--- scripts/add_buckets.ps1
+++ scripts/add_buckets.ps1
@@ -20,9 +20,10 @@ if ($buckets.count -ge 1) {
     }
     foreach($bucket in $buckets) {
         if($null -eq ($known_buckets | Where-Object {$_ -eq $bucket})) {
-            Write-Error `
-              "Bucket `"$bucket`" is not in known buckets ($known_buckets)." `
-              -ErrorAction Stop
+            Write-Error (
+                "Bucket `"$bucket`" is not in known buckets " +
+                "($($known_buckets -join ','))."
+            ) -ErrorAction Stop
         }
         Write-SetupScoopLog "Adding `"${bucket}`" bucket"
         Invoke-External -Command "scoop" -Parameters "bucket", "add", "$bucket"
