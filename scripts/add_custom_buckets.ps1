305ebbf2e733be49f44a57d2b158813cb08f860a
Minoru Sekine <Minoru.Sekine.JP@gmail.com>
2026-05-02 22:27:47 +0900

    feat: Support user own bucket referred by git repo URL. (#88)
    Closes #88

---
+++ scripts/add_custom_buckets.ps1
@@ -0,0 +1,53 @@
+param([string]$custom_buckets_string)
+
+Import-Module (Join-Path $($PSScriptRoot) "modules/Invoke-External")
+Import-Module (Join-Path $($PSScriptRoot) "modules/Test-Params")
+Import-Module (Join-Path $($PSScriptRoot) "modules/Write-SetupScoopLog")
+
+[string[]] $lines = @()
+if ($custom_buckets_string) {
+    $lines = (
+        $custom_buckets_string -split '\r?\n'
+        | ForEach-Object { $_.Trim() }
+        | Where-Object { $_ -ne "" }
+    )
+}
+# $custom_buckets_string, $lines, $line, and $repoUrl
+# shouldn't be output to log,
+# because they can be include HTTPS PAT or some secrets.
+if ($lines.count -ge 1) {
+    $i = 0
+    foreach($line in $lines) {
+        $i++
+        $found = $line | Select-String '\A(?<name>\S+)\s+(?<repo>.+)\z'
+        if (-not $found) {
+            Write-Error "Error in parsing line $i of $($lines.count)" `
+              -ErrorAction Stop
+        }
+        if (-not $found.Matches[0].Groups['name'].Success) {
+            Write-Error "Error in parsing name in line $i of $($lines.count)" `
+              -ErrorAction Stop
+        }
+        $name = $found.Matches[0].Groups['name'].Value
+        if ((Test-BucketName -bucketName $name) -eq $false) {
+            Write-Error "Bucket name ""$name"" is illegal." -ErrorAction Stop
+        }
+        if (-not $found.Matches[0].Groups['repo'].Success) {
+            Write-Error "Error in parsing repo in line $i of $($lines.count)" `
+              -ErrorAction Stop
+        }
+        $repoUrl = $found.Matches[0].Groups['repo'].Value
+        if ((Test-BucketRepoUrl -bucketRepoUrl $repoUrl) -eq $false) {
+            Write-Error @"
+Repo in line $i of $($lines.count) is illegal.
+`custom_buckets` parameter is for URL-styled repo bucket.
+"@ `
+              -ErrorAction Stop
+        }
+        Write-SetupScoopLog "Adding ""$name"""
+        Invoke-External `
+          -Command "scoop" `
+          -Parameters @("bucket", "add", "$name", "$repoUrl") `
+          -ErrorMessage """scoop bucket add $name"" failed."
+    }
+}
--- scripts/modules/Test-Params/Test-Params.psm1
+++ scripts/modules/Test-Params/Test-Params.psm1
@@ -1,3 +1,12 @@
+function Test-ScoopIdentifier {
+    [OutputType([bool])]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$identifier
+    )
+    return $identifier -imatch "\A\w[\w/.@-]*\z"
+}
+
 <#
 .SYNOPSIS
 
@@ -13,9 +22,65 @@ function Test-AppName {
     [OutputType([bool])]
     param(
         [Parameter(Mandatory = $true)]
-        [string]$AppName
+        [string]$appName
     )
-    return $AppName -imatch "\A\w[\w/.@-]*\z"
+    return Test-ScoopIdentifier -Identifier $appName
 }
 
-Export-ModuleMember -Function 'Test-AppName'
+<#
+.SYNOPSIS
+
+Test given string is a legal bucket name in scoop.
+
+.DESCRIPTION
+
+Test given string is a legal bucket name in scoop
+by regex.
+#>
+function Test-BucketName {
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$bucketName
+    )
+    return Test-ScoopIdentifier -Identifier $bucketName
+}
+
+function Test-LocalAbsoluteWindowsPath {
+    [OutputType([bool])]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$str
+    )
+    return ($str -imatch "\A[a-z]:.*")
+}
+
+<#
+.SYNOPSIS
+
+Test given string is a legal bucket remote repo in scoop.
+
+.DESCRIPTION
+
+Test given string is a legal bucket remote repo in scoop
+by regex.
+This function returns $false for local git repo.
+It will be supported by another way.
+#>
+function Test-BucketRepoUrl {
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$bucketRepoUrl
+    )
+    return (
+        ($bucketRepoUrl -imatch "\A\w[\w/.:@-]*\z") -and
+        (-not (Test-LocalAbsoluteWindowsPath $bucketRepoUrl)) -and
+        ($bucketRepoUrl -imatch ":")  # Remote repo requires colon.
+    )
+}
+
+Export-ModuleMember `
+  -Function 'Test-AppName', 'Test-BucketName', 'Test-BucketRepoUrl'
