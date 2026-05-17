e53c842dfb4bd9882915b015a03570aa2c4f4ed1
Minoru Sekine <Minoru.Sekine.JP@gmail.com>
2026-05-17 10:14:26 +0900

    feat: Refine features of `install_scoop` parameter. (#159)
    Closes #159

--- scripts/install_scoop.ps1
+++ scripts/install_scoop.ps1
@@ -1,5 +1,11 @@
-param([switch]$ForceAdmin)
+param(
+    [switch]$ForceAdmin,
+    [switch]$SkipIfAvailable
+)
 
+if ($SkipIfAvailable -and (Get-Command "scoop")) {
+    exit 0
+}
 $install_script = [scriptblock]::Create((Invoke-RestMethod -Uri https://get.scoop.sh))
 if ($ForceAdmin) {
     & $install_script -RunAsAdmin
