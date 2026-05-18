7098aa8daeb598014878886f8e0e160460ff2241
Minoru Sekine <Minoru.Sekine.JP@gmail.com>
2026-05-18 09:13:42 +0900

    chore: Output log when skipping `scoop` installation.

--- scripts/install_scoop.ps1
+++ scripts/install_scoop.ps1
@@ -3,7 +3,10 @@ param(
     [switch]$SkipIfAvailable
 )
 
+Import-Module (Join-Path $($PSScriptRoot) "modules/Write-SetupScoopLog")
+
 if ($SkipIfAvailable -and (Get-Command "scoop")) {
+    Write-SetupScoopLog "`scoop` is found. Skip installation."
     exit 0
 }
 $install_script = [scriptblock]::Create((Invoke-RestMethod -Uri https://get.scoop.sh))
